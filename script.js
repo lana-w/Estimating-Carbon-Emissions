0fd2f480d1902ca06ee8eee3f9353d1c5e8ef84e
Lana W <lanawehbeh12@gmail.com>
2024-08-16 14:14:46 -0400

    API Call - Estimate Carbon Impact

---
+++ script.js
@@ -0,0 +1,54 @@
+async function fetchData(num_passengers, distance){
+    const url = "https://api.climatiq.io/estimate";
+    const api_key = "16ZW4BTV6CMS08KFCRNHRKQXYCRG";
+
+    const auth_headers ={
+        "Authorization": "Bearer 16ZW4BTV6CMS08KFCRNHRKQXYCRG",
+        // "Authorization": `Bearer ${api_key}`,
+    }
+
+    const params = {
+        "emission_factor": {
+            "activity_id": "passenger_flight-route_type_domestic-aircraft_type_na-distance_na-class_na-rf_excluded-distance_uplift_included",
+            "source": "BEIS",
+            "region": "GB",
+            "year": 2021,
+            "source_lca_activity": "fuel_combustion",
+            "data_version": "15.15"
+        },
+        "parameters": {
+            "passengers": num_passengers,
+            "distance": distance,
+            "distance_unit": "km"
+        }
+    }
+    
+    try{
+        const response = await fetch(url, {
+            method: "POST",
+            headers: auth_headers,
+            body: JSON.stringify(params)
+        }
+        );
+
+        const data = await response.json();
+        
+        const {
+            co2e,
+            co2e_unit
+        } = data
+
+
+        console.log(data);
+        console.log(co2e);
+        console.log(co2e_unit);
+        return data;
+    }
+
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+fetchData();
